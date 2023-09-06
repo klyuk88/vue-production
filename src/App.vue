ba6b9f0eb9560f1049ffb867b574ade6ff2c648e
Илья <klyuk88@MacBook-Air-Ila.local>
2023-09-06 06:11:35 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import TheAside from "@/components/widgets/TheAside.vue";
+import TheHeader from "@/components/widgets/TheHeader.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main>
+    <TheAside/>
+    <div class="w-full fixed h-full top-0 left-0 bg-black bg-opacity-50 z-10"></div>
+    <div id="page" class="ml-[250px]">
+      <TheHeader/>
+      <RouterView />
     </div>
-  </header>
+  </main>
 
-  <RouterView />
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/widgets/TheHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const isShow = ref(false)
+
+</script>
+<template>
+ <header class="flex gap-16 border-2 p-5 sticky top-0 bg-neutral-50">
+   <h1>Logo</h1>
+   <ul class="flex items-center gap-4">
+     <li>1</li>
+     <li>2</li>
+     <li>3</li>
+     <li>4</li>
+     <li>5</li>
+   </ul>
+   <div class="w-8 h-8 bg-amber-600 rounded-full ml-auto cursor-pointer relative z-20" @click="isShow = true"></div>
+   <div class="w-full fixed h-full top-0 left-0 bg-black bg-opacity-50 z-10" v-if="isShow" @click.self="isShow = false"></div>
+ </header>
+</template>
