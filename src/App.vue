1dc08c9ae47d4ec7be6f379056df8b4aec81b3c8
klyuk88 <klyukovskiy@yandex.ru>
2023-11-12 14:48:55 +0300

    add vue use Fetch

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import TheAside from "@/components/widgets/TheAside.vue";
 import TheHeader from "@/components/widgets/TheHeader.vue";
 </script>
 
 <template>
   <main>
-    <!--    <TheAside />-->
     <div id="page" class="">
       <TheHeader />
-      <router-view></router-view>
+      <Suspense>
+        <router-view></router-view>
+      </Suspense>
     </div>
   </main>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,76 @@
 <script setup lang="ts">
-import UiSelect from "@/components/Ui/UiSelect/UiSelect.vue";
-import type { TIUiSelectOption } from "@/components/Ui/UiSelect/types";
+import { createFetch, type UseFetchOptions } from "@vueuse/core";
+import queryString from "query-string";
+import { computed, reactive } from "vue";
 
-const options2: TIUiSelectOption[] = [
-  {
-    label: "Ilya",
-    category: "Ru"
-  },
-  {
-    label: "Nick",
-    category: "Ru"
-  },
-  {
-    label: "Bob",
-    category: "En"
-  },
-  {
-    label: "Anton",
-    category: "En"
-  }
-];
+type TPost = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
-const selectHandler = (item: TIUiSelectOption | TIUiSelectOption[]) => {
-  if (Array.isArray(item)) {
-    console.log(item);
-  } else {
-    console.log(item.label);
-  }
+const configQuery = reactive({
+  _limit: 3,
+  _page: 1
+});
+
+const useMyFetch = <T,>(
+  url: string,
+  queryParams?: Record<string, any>,
+  useFetchOptions: UseFetchOptions = {}
+) => {
+  //create instance
+  const fetch = createFetch({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+    options: {
+      refetch: true
+    }
+  });
+  //ref url
+  const urlRef = computed<string>(() => {
+    return `${url}${
+      queryParams
+        ? "?" +
+          queryString.stringify(queryParams, {
+            arrayFormat: "comma"
+          })
+        : ""
+    }`;
+  });
+  // return instance
+  return fetch<T>(urlRef, {
+    ...useFetchOptions
+  });
+};
+
+const { data, error, isFetching } = useMyFetch<TPost[]>("/posts", configQuery, {
+  refetch: false
+});
+
+const setPageHandler = () => {
+  configQuery._page = configQuery._page + 1;
+};
+const addPost = () => {
+  useMyFetch<TPost>("/posts").post({
+    title: "foo",
+    body: "bar",
+    userId: 1
+  });
 };
 </script>
 <template>
   <div id="home-page" class="px-10 pt-5 container mx-auto">
     <h1>Home page</h1>
     <div class="w-[400px]">
-      <UiSelect
-        :options="options2"
-        @change="selectHandler"
-        :multiple="false"
-        :searchable="false"
-        :grouping="true"
-      >
-      </UiSelect>
+      <div v-if="error">Error</div>
+      <div v-else-if="isFetching">...Loading</div>
+      <div v-else>
+        <pre>{{ data }}</pre>
+      </div>
+
+      <button @click="setPageHandler">Set page</button>
+      <button @click="addPost">Add Post</button>
     </div>
   </div>
 </template>
